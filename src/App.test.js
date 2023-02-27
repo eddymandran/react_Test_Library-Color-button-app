b1e26ac7454b490350709d293a6612bbd18545ac
Eddy M <59568524+eddymandran@users.noreply.github.com>
2023-02-27 11:52:45 +0100

    add logRoles to see the roles in the console

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import {logRoles} from '@testing-library/dom'
+
 import App from './App';
 
 test('button has correct initial color', () => {
-render(<App/>);
+
+// add log roles to see the roles of the element
+const {container} = render(<App/>);
+logRoles(container)
 
 //find an element with a role of button and text of 'Change to blue'
 const colorButton = screen.getByRole('button', {name : 'Change to blue'});
